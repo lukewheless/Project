d76724e617e4e119e5b643cc15248c81ab92bc93
Wheless <Luke_Wheless@baylor.edu>
2020-11-18 15:54:07 -0600

    first commit

---
+++ run.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+
+# import data
+strava = pd.read_csv("Strava Data.csv")
+df = pd.DataFrame(strava)
+
+def speed(col):
+    if col['avg_pace'] >= 07:10:
+        return "Fast"
+    else:
+        return "Slow"
+
+df['Speed'] = df.apply(speed, axis=1)
+
+# extracting seconds from time  
+df['seconds'] = df['Time'].dt.second
+df['avg_seconds'] = df['Avg. Pace'].dt.second
+
+# displaying DataFrame with new seconds columns
+display(df)
+
+# moving df to a new csv
+df.to_csv("Strava.csv")
