3c1f1829b96b9f35bda7d20986458b0c4ac3b08a
Luke Wheless <lukewheless@Lukes-MacBook-Air.local>
2020-11-20 16:33:39 -0600

    enter

--- run.py
+++ run.py
@@ -4,23 +4,25 @@ import numpy as np
 # import data
 strava = pd.read_csv("StravaData.csv")
 df = pd.DataFrame(strava)
+print(df)
 
+# manipulate dataset
 df['Ave_Pace'] = (df['Time']/df['Distance (m)'])
 
+# extracting seconds from time  
+df['Total_Seconds'] = df['Time'].dt.second
+df['Avg_Seconds'] = df['Avg_Pace'].dt.second
+
 def speed(col):
-    if col['Ave_Pace'] >= 7:
+    if col['Avg_Seconds'] >= 430: # 7:10 time
         return "Fast"
     else:
         return "Slow"
 
 df['Speed'] = df.apply(speed, axis=1)
 
-# extracting seconds from time  
-df['Seconds'] = df['Time'].dt.second
-df['Avg_Seconds'] = df['avg_pace'].dt.second
-
-# displaying DataFrame with new seconds columns
+# displaying DataFrame with new columns
 print(df)
 
 # moving df to a new csv
-#df.to_csv("Strava.csv")
+df.to_csv("Strava.csv")
